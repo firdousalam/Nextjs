9c13af4a12755c17dde5c6ba182dfd618d5f8d03
firdous alam <firdous.alam2058@gmail.com>
2023-05-26 19:40:30 +0530

    new nextjs project setup

---
+++ frontend/technophile/src/app/movie/page.js
@@ -0,0 +1,12 @@
+"use client"// this think you need to add
+import React, { Component } from 'react'
+
+export class page extends Component {
+  render() {
+    return (
+      <div>Movie page</div>
+    )
+  }
+}
+
+export default page
