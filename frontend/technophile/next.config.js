76f7323e492bebdc439005e00f00a5c53e3d373c
firdous alam <firdous.alam2058@gmail.com>
2023-05-28 12:26:42 +0530

    adding dynamic route and http calls

--- frontend/technophile/next.config.js
+++ frontend/technophile/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+  env: {
+    URL: process.env.URL,
+    HOST : process.env.HOST,
+    KEY : process.env.KEY
+  }
+
+}
 
 module.exports = nextConfig
--- frontend/technophile/src/app/movie/page.js
+++ frontend/technophile/src/app/movie/page.js
@@ -1,9 +1,26 @@
-"use client"// this think you need to add
+// this think you need to add
+"use client"
 import Link from 'next/link'
 import React, { Component } from 'react'
+const Movie = async ()=> {
+  console.log(process.env.URL);
+  await new Promise(resolve => setTimeout(resolve, 2000));
 
-export class page extends Component {
-  render() {
+
+  const url = process.env.URL;
+
+  const options = {
+      method: 'GET',
+      headers: {
+          'X-RapidAPI-Key': process.env.KEY,
+          'X-RapidAPI-Host': process.env.HOST
+      }
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+  console.log(main_data)
     return (
       <>
         <h1>Welcome to Movie Page</h1>
@@ -11,6 +28,5 @@ export class page extends Component {
       </>
     )
   }
-}
 
-export default page
+export default Movie
